d5569a11b1ad972e2e14e4480eb2f4dcfff37687
Igor <deshchenkoigor@gmail.com>
2024-04-30 20:14:05 +0300

    js-hw1

---
+++ js-lesson1-hw/scripts/main.js
@@ -0,0 +1,36 @@
+let w1 = 'Hello';
+let w2 = 'owu';
+let w3 = 'com';
+let w4 = 'ua';
+
+console.log(w1,w2,w3,w4);
+
+
+
+let e=1;
+let f=10;
+let g=-999;
+let h=123;
+let i=3.14;
+let j=2.7;
+let k=16;
+let l=true;
+let m=false;
+
+console.log(e,f,g,h,i,j,k,l,m);
+
+
+let firstName = 'Ігор';
+let middleName = 'Олександрович';
+let lastName = 'Дещенко';
+result = `${firstName} ${middleName} ${lastName}`;
+
+console.log(result);
+
+
+let a =100;
+let b ='100';
+let c = true;
+console.log(typeof a,b,c);
+
+
